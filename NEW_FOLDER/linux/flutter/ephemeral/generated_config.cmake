1627aa5cdeb55830109c698770cf1288ac499eb2
Ezra Huang <ezrahuang155@gmail.com>
2022-03-20 22:17:36 -0600

    reverting attempted project rename

---
+++ NEW_FOLDER/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ezrahuang/.local/lib/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ezrahuang/Projects/luples/luples_flutter" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ezrahuang/.local/lib/flutter"
+  "PROJECT_DIR=/home/ezrahuang/Projects/luples/luples_flutter"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ezrahuang/Projects/luples/luples_flutter/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ezrahuang/Projects/luples/luples_flutter/lib/main.dart"
+)
